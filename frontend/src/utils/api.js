3689f81bc8281173fce48c683cdb372f907969d9
chejoGuzman <sguzman@launion.com.gt>
2024-05-23 20:13:47 -0600

    Deploy

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -6,8 +6,8 @@ const PROD = 'PROD'
 
 const environ = process.env.REACT_APP_BUILD_ENV || LOCAL
 
-const HOST = 'http://localhost:5000/api/'// LOCAL
-// const HOST = 'https://pcaycdpjt6.execute-api.us-east-1.amazonaws.com/dev/api/' // DESARROLLO
+// const HOST = 'http://localhost:5000/api/'// LOCAL
+const HOST = 'https://pcaycdpjt6.execute-api.us-east-1.amazonaws.com/dev/api/' // DESARROLLO
 
 class Api {
     async Get(uri, params = null) {
